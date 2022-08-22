dc20fbe781e5577b971fc95f6dbe6d546a5a2123
Juliya Boiko <boikojuliya2707@gmail.com>
2022-08-23 00:41:24 +0400

    editContact

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -3,7 +3,6 @@ import { getAllContactsAsync } from "redux/contacts/cont-operations";
 import { useEffect } from "react";
 import { getItems } from "redux/contacts/cont-selectors";
 import { ContactCard } from "./ContactCard";
-import { fetchEditContact } from "api/axios";
 
 export const ContactsList = () => {
     const dispatch = useDispatch();
@@ -13,11 +12,6 @@ export const ContactsList = () => {
     dispatch(getAllContactsAsync());
   }, [dispatch]);
 
-  // console.log(fetchEditContact({
-  //   id: '6303dec88aa5ec001529fe3c',
-  //   name: 'fff',
-  //   number: '444'
-  // }));
   return (
     <div>
       <ul>
