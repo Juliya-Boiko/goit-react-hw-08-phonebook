6f2fae9e2347345d62901f49d3a876b026000efe
Juliya Boiko <boikojuliya2707@gmail.com>
2022-08-23 22:12:46 +0400

    UserBar styled

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -1,6 +1,7 @@
 import { useRedux } from "hooks/useRedux";
-import { NavLink } from "react-router-dom";
 import { logoutUser, getUserName, getLogging } from "redux/authSlice";
+import { NavLink } from "react-router-dom";
+import { UserBarInfo, UserName, LogoutButton } from "./UserBar.styled";
 
 export const UserBar = () => {
   const [useSelector, dispatch] = useRedux();
@@ -14,10 +15,10 @@ export const UserBar = () => {
   return (
     <>
       {isLogged
-        ? <div>
-            Hello, {name} !
-            <button type="button" onClick={logoutHandler}>Log out</button>
-          </div> 
+        ? <UserBarInfo>
+            <UserName>You are logged in as: {name}</UserName>
+            <LogoutButton type="button" onClick={logoutHandler}>EXIT</LogoutButton>
+          </UserBarInfo> 
         : <ul>
             <li>
                 <NavLink to="/register">Register</NavLink>
--- src/components/UserBar/UserBar.styled.js
+++ src/components/UserBar/UserBar.styled.js
@@ -1,2 +1,31 @@
 import styled from "styled-components";
 
+export const UserBarInfo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const UserName = styled.p`
+  margin-right: 10px;
+  font-size: 14px;
+  color: ${p => p.theme.colors.white};
+
+  @media screen and (max-width: 425px) {
+    font-size: 9px;
+  }
+`;
+
+export const LogoutButton = styled.button`
+display: block;
+padding: 5px 20px;
+border: none;
+border-radius: 2px;
+outline: 1px solid ${p => p.theme.colors.white};
+background-color: transparent;
+color: ${p => p.theme.colors.white};
+
+@media screen and (max-width: 425px) {
+  padding: 3px 10px;
+  font-size: 9px;
+}
+`;
